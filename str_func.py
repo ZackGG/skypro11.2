9e613994c0e9db9c772661912478ca27dac214a4
Zack <kur.uzak94@gmail.com>
2023-05-23 02:52:37 +0800

    новая фукнция

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
 def all_capital(word):
+    """Эта функция возвращает все большие буквы строки"""
     return word.upper()
 
+def first_capital(word):
+    """
+    Возвращает первую большую букву
+    """
+
+    return word.capitalize()
